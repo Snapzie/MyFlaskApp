9d5d274a5a14edbf13aa2483feac7586c9636123
kaapsi <casper@bresdahl.dk>
2019-02-09 22:45:49 +0100

    Initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title':'Article One',
+      'body':'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum.',
+      'author':'Brad Traversy',
+      'create_date':'04-25-2017'
+    },
+    {
+      'id': 2,
+      'title':'Article Two',
+      'body':'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum.',
+      'author':'Brad Traversy',
+      'create_date':'04-25-2017'
+    },
+    {
+      'id': 3,
+      'title':'Article Three',
+      'body':'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum.',
+      'author':'Brad Traversy',
+      'create_date':'04-25-2017'
+    }
+  ]
+  return articles
